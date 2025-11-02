54b34870d5929bf1bf3b3fc970c86062a68b6adf
Abhijeet yadav <145228365+abhijeet5642@users.noreply.github.com>
2025-11-02 16:12:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://devfolio-o8xs.onrender.com
-' || 'http://localhost:5173',
-  credentials: true
+  origin: ['https://devfolio-o8xs.onrender.com', 'http://localhost:5173'],
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
